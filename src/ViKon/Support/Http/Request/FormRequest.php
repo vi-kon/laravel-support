92dace9f5f5eff57ebe10a29ad94f99ae79eb5b4
Kovács Vince <vincekovacs@hotmail.com>
2015-11-12 09:22:25 +0100

    Added FormRequest

---
+++ src/ViKon/Support/Http/Request/FormRequest.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace ViKon\Support\Http\Request;
+
+/**
+ * Class FormRequest
+ *
+ * @package ViKon\Support\Http\Request
+ *
+ * @author  Kovács Vince<vincekovacs@hotmail.com>
+ *
+ * @method array rules() Get the validation rules that apply to the request.
+ * @method bool authorize() Determine if the user is authorized to make this request.
+ */
+class FormRequest extends \Illuminate\Foundation\Http\FormRequest
+{
+
+}
